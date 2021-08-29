6abd94e546de19e26ea651b605d6f165c323ca58
Sandy <lgsandy543@gmail.com>
2021-08-29 23:17:04 +0530

    Routes implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home.dart';
+import 'package:flutter_application_1/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Home(),
+        "/login": (context) => Login(),
+        "/home": (context) => Home()
+      },
+    );
   }
 }
