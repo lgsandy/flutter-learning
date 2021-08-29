a0f23b11091ed4237cce7bce28289469a8848e21
Sandy <lgsandy543@gmail.com>
2021-08-29 22:28:02 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello from flutter app"),
+        ),
+      ),
+    );
+  }
+}
