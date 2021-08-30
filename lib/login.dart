e5e0ee1063a6cac8c06ed39190f49611ee0ae0c6
Sandy <lgsandy543@gmail.com>
2021-08-30 22:34:05 +0530

    Day login page is designed.

--- lib/login.dart
+++ lib/login.dart
@@ -5,11 +5,45 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("login screen"),
-        ),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "welcome",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 36),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "User Name", hintText: "Enter User name"),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: "Password", hintText: "Enter Password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login"))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
